ec83fc030ad7c098637df6f3a56d844f8526c571
Aimé Bertrand <aime.bertrand@macowners.club>
2024-02-24 21:24:50 +0100

    Add support for `outline-minor-faces'

--- timu-caribbean-theme.el
+++ timu-caribbean-theme.el
@@ -1601,6 +1601,16 @@ Sourced other themes to get information about font faces for packages.")
    `(outline-7 ((,class (:foreground ,purple))))
    `(outline-8 ((,class (:foreground ,fg))))
 
+;;;; outline-minor-faces
+     `(outline-minor-1 ((,class (:foreground ,blue :weight ultra-bold ,@(timu-caribbean-do-scale timu-caribbean-scale-org-document-info 1.2)))))
+     `(outline-minor-2 ((,class (:foreground ,teal :weight bold ,@(timu-caribbean-do-scale timu-caribbean-scale-org-document-info 1.2)))))
+     `(outline-minor-3 ((,class (:foreground ,red :weight bold ,@(timu-caribbean-do-scale timu-caribbean-scale-org-document-info 1.1)))))
+     `(outline-minor-4 ((,class (:foreground ,cyan))))
+     `(outline-minor-5 ((,class (:foreground ,green))))
+     `(outline-minor-6 ((,class (:foreground ,orange))))
+     `(outline-minor-7 ((,class (:foreground ,purple))))
+     `(outline-minor-8 ((,class (:foreground ,fg))))
+
 ;;;; parenface
    `(paren-face ((,class (:foreground ,caribbean5))))
 
